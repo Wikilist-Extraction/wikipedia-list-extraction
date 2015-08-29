9f149333bd92aa74d6f734453b50b8a253141c6b
Nico Ring <ringnico@web.de>
2015-08-29 12:15:34 +0200

    comments out kmeans method due to compiler errors

--- src/main/scala/filtering/KMeansFilterStrategy.scala
+++ src/main/scala/filtering/KMeansFilterStrategy.scala
@@ -47,35 +47,36 @@ object KMeans {
 
   def cluster[T,U](xs: Seq[T], k: Int)
                   (implicit projection: T => U, space: VectorSpace[U]): Seq[Seq[T]] = {
-    case class Pair(original: T) {
-      val projected = projection(original)
-    }
-    @tailrec
-    def step(xs: Seq[Pair], centroids: Seq[U]): Seq[Seq[Pair]] = {
-      val labeled =
-        for (x <- xs) yield {
-          val distances = for ((centroid) <- centroids) yield (centroid, space.distance(x.projected, centroid))
-          val nearestCentroid = distances.minBy(_._2)._1
-          (nearestCentroid, x)
-        }
-      val grouped = for (centroid <- centroids) yield labeled.collect({
-        case (`centroid`, x) => x
-      })
-      val replacements = grouped.map(group => space.centroid(group.map(_.projected)))
-      val stable =
-        replacements.forall {
-          replacement =>
-            centroids.contains(replacement)
-        }
-      if (stable) {
-        grouped
-      } else {
-        step(xs, replacements)
-      }
-    }
-    val associated = xs.map(Pair)
-    val initial = pickRandom(associated.map(_.projected), k)
-    step(associated, initial).map(_.map(_.original))
+//    case class Pair(original: T) {
+//      val projected = projection(original)
+//    }
+//    @tailrec
+//    def step(xs: Seq[Pair], centroids: Seq[U]): Seq[Seq[Pair]] = {
+//      val labeled =
+//        for (x <- xs) yield {
+//          val distances = for ((centroid) <- centroids) yield (centroid, space.distance(x.projected, centroid))
+//          val nearestCentroid = distances.minBy(_._2)._1
+//          (nearestCentroid, x)
+//        }
+//      val grouped = for (centroid <- centroids) yield labeled.collect({
+//        case (`centroid`, x) => x
+//      })
+//      val replacements = grouped.map(group => space.centroid(group.map(_.projected)))
+//      val stable =
+//        replacements.forall {
+//          replacement =>
+//            centroids.contains(replacement)
+//        }
+//      if (stable) {
+//        grouped
+//      } else {
+//        step(xs, replacements)
+//      }
+//    }
+//    val associated = xs.map(Pair)
+//    val initial = pickRandom(associated.map(_.projected), k)
+//    step(associated, initial).map(_.map(_.original))
+    List()
   }
 
   def cluster[T,U](fn: T => U)(xs: Seq[T], k: Int)
